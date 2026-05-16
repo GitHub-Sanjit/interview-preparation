a60813fca4e9ab9ef291f364bb75c61e912046c6
sanjit <sanjit.est14@gmail.com>
2026-05-16 23:50:03 +0600

    Anagram problem, frequency counter problem

---
+++ attempt1/day1/practice_2.js
@@ -0,0 +1,22 @@
+function anagram(s, t) {
+  if (s.length !== t.length) {
+    return false;
+  }
+
+  const map = new Map();
+  for (let char of s) {
+    map.set(char, (map.get(char) || 0) + 1);
+  }
+
+  for (let char of t) {
+    if (!map.has(char)) return false;
+
+    map.set(char, map.get(char) - 1);
+
+    if (map.get(char) < 0) return false;
+  }
+  return true;
+}
+
+const result = anagram("anagram", "nagaram")
+console.log(result)
