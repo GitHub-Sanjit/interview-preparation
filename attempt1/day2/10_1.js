d373907476f3f96ef78dc7551f2c29aa6a548ef8
sanjit <sanjit.est14@gmail.com>
2026-05-18 20:52:38 +0600

    containDuplicate

---
+++ attempt1/day2/10_1.js
@@ -0,0 +1,11 @@
+function containDuplicate(nums) {
+  const set = new Set();
+
+  for (let num of nums) {
+    if (set.has(num)) return true;
+    set.add(num);
+  }
+  return false
+}
+
+console.log(containDuplicate([1, 2, 3]));
